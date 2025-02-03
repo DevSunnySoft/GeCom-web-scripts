e8aff383d55097275303f6e3a031902729d1a1c5
Jefferson <jeffersongvmatos@gmail.com>
2025-02-03 13:38:08 -0300

    Added script 0.8

--- 0.8/script 0.8.sql
+++ 0.8/script 0.8.sql
@@ -1,8 +1,8 @@
--- Table: public.sales_promotions
+-- Table: public.sales_campaigns
 
-DROP TABLE IF EXISTS public.sales_promotions;
+DROP TABLE IF EXISTS public.sales_campaigns;
 
-CREATE TABLE IF NOT EXISTS public.sales_promotions
+CREATE TABLE IF NOT EXISTS public.sales_campaigns
 (
     _id uuid NOT NULL,
     created_at timestamp without time zone NOT NULL,
@@ -13,29 +13,29 @@ CREATE TABLE IF NOT EXISTS public.sales_promotions
     end_date timestamp without time zone,
     keywords character varying COLLATE pg_catalog."default" NOT NULL,
     company_id uuid,
-    CONSTRAINT sales_promotions_pkey PRIMARY KEY (_id)
+    CONSTRAINT sales_campaigns_pkey PRIMARY KEY (_id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.sales_promotions
+ALTER TABLE IF EXISTS public.sales_campaigns
     OWNER to postgres;
 
--- Table: public.sales_promotion_items
-DROP TABLE IF EXISTS public.sales_promotion_items;
+-- Table: public.sales_campaign_items
+DROP TABLE IF EXISTS public.sales_campaign_items;
 
-CREATE TABLE IF NOT EXISTS public.sales_promotion_items
+CREATE TABLE IF NOT EXISTS public.sales_campaign_items
 (
     _id uuid NOT NULL,
-    sales_promotion_id uuid NOT NULL,
+    sales_campaign_id uuid NOT NULL,
     product_variation_id uuid NOT NULL,
     sale_value numeric(12,2) NOT NULL DEFAULT '0'::numeric,
     created_at timestamp without time zone NOT NULL DEFAULT 'now()',
     updated_at timestamp without time zone,
     is_active boolean DEFAULT 'true',
     product_id uuid NOT NULL,
-    CONSTRAINT sales_promotion_prices_pkey PRIMARY KEY (_id),
-    CONSTRAINT fk_sales_promotion_prices_item_id FOREIGN KEY (product_variation_id)
+    CONSTRAINT sales_campaign_prices_pkey PRIMARY KEY (_id),
+    CONSTRAINT fk_sales_campaign_prices_item_id FOREIGN KEY (product_variation_id)
         REFERENCES public.product_variations (_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS public.sales_promotion_items
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.sales_promotion_items
+ALTER TABLE IF EXISTS public.sales_campaign_items
     OWNER to postgres;
 
 INSERT INTO public.versions values (gen_random_uuid(), CURRENT_TIMESTAMP, null, 8, '0.8');
